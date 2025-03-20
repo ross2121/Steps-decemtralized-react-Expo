16061a27fd1ef086983ddf1b948be61e5c958b33
Rupoum <rupoummitra15278@gmail.com>
2025-03-20 16:15:15 +0530

    Add balance modal

--- components/screens/Wallet.tsx
+++ components/screens/Wallet.tsx
@@ -13,7 +13,13 @@ import {
 } from "react-native";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import BottomSheet, {
+  BottomSheetBackdrop,
+  BottomSheetFlatList,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
@@ -99,9 +105,18 @@ const Wallet = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   const snapPoints = useMemo(() => ["25%", "50%", "75%"], []);
+  const snapPointsModal = useMemo(() => ["30%", "50%"], []);
 
-  const handleSheetChanges = useCallback((index: number) => {
-    // console.log("Bottom sheet index:", index);
+  // const handleSheetChanges = useCallback((index: number) => {
+  //   // console.log("Bottom sheet index:", index);
+  // }, []);
+
+  // Make sure we're using the correct type for the ref
+  // const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // This function should present the modal
+  const handleAdd = useCallback(() => {
+    bottomSheetModalRef.current?.present();
   }, []);
 
   const renderItem = useCallback(({ item }: any) => {
@@ -147,84 +162,200 @@ const Wallet = () => {
     );
   }, []);
 
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <StatusBar barStyle="light-content" /> */}
-      <LinearGradient
-        colors={["#1a0033", "#4b0082", "#290d44"]}
-        style={styles.gradient}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <LinearGradient
+            colors={["#1a0033", "#4b0082", "#290d44"]}
+            style={styles.gradient}
+          >
+            {/* Wallet Header */}
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Wallet</Text>
+              <TouchableOpacity style={styles.settingsButton}>
+                <Feather name="settings" size={24} color="#5EDCF5" />
+              </TouchableOpacity>
+            </View>
+
+            {/* Balance Display */}
+            <View style={styles.balanceContainer}>
+              <Text style={styles.balanceText}>
+                ${(balance / LAMPORTS_PER_SOL).toFixed(2)}
+              </Text>
+            </View>
+
+            {/* Action Buttons */}
+            <View style={styles.actionButtonsContainer}>
+              <TouchableOpacity
+                style={styles.actionButton}
+                onPress={handlePresentModalPress}
+              >
+                <Feather name="plus" size={24} color="white" />
+                <Text style={styles.actionButtonText}>Add</Text>
+              </TouchableOpacity>
+              {/* <Button title="Present Modal" color="black" /> */}
+              <BottomSheetModal
+                ref={bottomSheetModalRef}
+                snapPoints={snapPointsModal}
+                index={0}
+                handleIndicatorStyle={{
+                  backgroundColor: "#CCCCCC",
+                  width: 40,
+                  height: 5,
+                  borderRadius: 3,
+                }}
+                onChange={handleSheetChanges}
+                backgroundStyle={styles.bottomModalBackground}
+                backdropComponent={(props) => (
+                  <BottomSheetBackdrop
+                    {...props}
+                    disappearsOnIndex={-1}
+                    appearsOnIndex={0}
+                    opacity={0.9}
+                  />
+                )}
+              >
+                <BottomSheetView>
+                  <AddModal />
+                </BottomSheetView>
+              </BottomSheetModal>
+              <TouchableOpacity style={styles.actionButton}>
+                <Feather name="repeat" size={24} color="white" />
+                <Text style={styles.actionButtonText}>Swap</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.actionButton}>
+                <Feather name="send" size={24} color="white" />
+                <Text style={styles.actionButtonText}>Send</Text>
+              </TouchableOpacity>
+            </View>
+          </LinearGradient>
+
+          {/* Add Modal Bottom Sheet */}
+
+          {/* Bottom Sheet for Transactions */}
+          <BottomSheet
+            ref={bottomSheetRef}
+            index={0}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}
+            backgroundStyle={styles.bottomSheetBackground}
+            handleIndicatorStyle={styles.bottomSheetIndicator}
+            style={{ paddingHorizontal: 12 }}
+          >
+            <View
+              style={{
+                backgroundColor: "#1a0033",
+                marginTop: 20,
+                borderRadius: 20,
+              }}
+            >
+              <View style={styles.bottomSheetHeader}>
+                <Text style={styles.bottomSheetTitle}>Balances</Text>
+                <Feather name="clock" size={20} color="white" />
+              </View>
+
+              <BottomSheetFlatList
+                data={TRANSACTIONS}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                contentContainerStyle={styles.transactionList}
+              />
+            </View>
+          </BottomSheet>
+        </BottomSheetModalProvider>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+};
+
+const AddModal = () => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+      }}
+    >
+      <Text
+        style={{
+          color: "white",
+          fontSize: 24,
+        }}
       >
-        {/* Wallet Header */}
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>Wallet</Text>
-          <TouchableOpacity style={styles.settingsButton}>
-            <Feather name="settings" size={24} color="#5EDCF5" />
-          </TouchableOpacity>
-        </View>
-
-        {/* Balance Display */}
-        <View style={styles.balanceContainer}>
-          <Text style={styles.balanceText}>
-            ${(balance / LAMPORTS_PER_SOL).toFixed(2)}
-          </Text>
-        </View>
-
-        {/* Action Buttons */}
-        <View style={styles.actionButtonsContainer}>
-          <TouchableOpacity style={styles.actionButton} onPress={onClick}>
-            <Feather name="plus" size={24} color="white" />
-            <Text style={styles.actionButtonText}>Add</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.actionButton}>
-            <Feather name="repeat" size={24} color="white" />
-            <Text style={styles.actionButtonText}>Swap</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.actionButton}>
-            <Feather name="send" size={24} color="white" />
-            <Text style={styles.actionButtonText}>Send</Text>
-          </TouchableOpacity>
-        </View>
-      </LinearGradient>
-
-      {/* Bottom Sheet */}
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={0}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}
-        backgroundStyle={styles.bottomSheetBackground}
-        handleIndicatorStyle={styles.bottomSheetIndicator}
-        style={{ paddingHorizontal: 12 }}
+        Add crypto
+      </Text>
+      <View
+        style={{
+          padding: 20,
+          borderRadius: 20,
+        }}
       >
         <View
           style={{
-            backgroundColor: "#1a0033",
-            marginTop: 20,
+            backgroundColor: "#9c96a3",
+            padding: 10,
             borderRadius: 20,
+            marginTop: 20,
           }}
         >
-          <View style={styles.bottomSheetHeader}>
-            <Text style={styles.bottomSheetTitle}>Balances</Text>
-            <Feather name="clock" size={20} color="white" />
-          </View>
-
-          <BottomSheetFlatList
-            data={TRANSACTIONS}
-            keyExtractor={(item) => item.id}
-            renderItem={renderItem}
-            contentContainerStyle={styles.transactionList}
-          />
+          <Text
+            style={{
+              color: "white",
+            }}
+            numberOfLines={1}
+            ellipsizeMode="tail"
+          >
+            sdtyuioujgfdxcgvhbjkjhbxvcvghjkljdfghijjihgcvbnm,nbvhjkokuytfyuijlkbn
+            m,k.lkytyu
+          </Text>
         </View>
-      </BottomSheet>
-    </SafeAreaView>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#7E3887",
+            paddingVertical: 10,
+            width: "20%",
+            borderRadius: 20,
+            marginTop: 20,
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+          onPress={() => {
+            const textToCopy =
+              "sdtyuioujgfdxcgvhbjkjhbxvcvghjkljdfghijjihgcvbnm,nbvhjkokuytfyuijlkbnm,k.lkytyu";
+            navigator.clipboard.writeText(textToCopy).then(() => {
+              Alert.alert("Copied to clipboard!");
+            });
+          }}
+        >
+          <Text style={{ color: "white" }}>Copy</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   gradient: {
     flex: 1,
+  },
+  // modal
+  bottomModalBackground: {
+    flex: 1,
+    backgroundColor: "#1a0033",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
   },
   container: {
     flex: 1,
@@ -284,8 +415,10 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 24,
   },
   bottomSheetIndicator: {
-    backgroundColor: "CCCCCC",
+    backgroundColor: "#CCCCCC",
     width: 40,
+    height: 5,
+    borderRadius: 3,
   },
   bottomSheetHeader: {
     flexDirection: "row",
